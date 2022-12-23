0208d679db667c373c5ba5a41ced36a3a3607f98
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-12-23 19:44:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,14 @@ from pyrogram import Client
 import os
 from aiohttp import web
 from plugins import web_server
+app = web.AppRunner(await web_server())
 
+        await app.setup()
+
+        bind_address = "0.0.0.0"
+
+        await web.TCPSite(app, bind_address, 8000).start()
+        
 from config import Config
 
 
