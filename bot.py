ff8cc01b650f04950466b843cc1f808ff68f05c7
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-12-24 10:52:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,47 @@ app = web.AppRunner(await web_server())
         bind_address = "0.0.0.0"
 
         await web.TCPSite(app, bind_address, 8000).start()
+        class Bot(Client):
+
+    def init(self):
+
+        super().init(
+
+        "renamer",
+
+        api_id=API_ID,
+
+        api_hash=API_HASH,
+
+        bot_token=BOT_TOKEN,
+
+        plugins=dict(root="plugins"),
+
+        workers=50,
+
+        sleep_threshold=10
+
+        )
+
+    async def start(self):
+
+        await super().start()
+
+        #web-response
+
+        app = web.AppRunner(await web_server())
+
+        await app.setup()
+
+        bind_address = "0.0.0.0"
+
+        await web.TCPSite(app, bind_address, 8000).start()
+
+        print('Bot started')
+
+    async def stop(self, *args):
+
+        await super().stop()
         
 from config import Config
 
